9e3065eb40c3e4a66808bfb8fdc7902b9ceea488
lukjcnWq <18819052272@163.com>
2024-11-25 21:28:20 +0800

    初始化项目

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#f0f9ff',
+          100: '#e0f2fe',
+          // ... 添加自定义颜色
+        }
+      },
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    },
+  },
+  plugins: [],
+}
